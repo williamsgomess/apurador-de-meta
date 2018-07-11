fa8c99ee681b3fce8109f6bd5ca6a7bb472379e2
Williams Gomes <williamsgomess@icloud.com>
2018-07-11 13:44:03 -0300

    iniciando trabalhos

---
+++ src/main/java/br/com/centrocar/apurador/main/App.java
@@ -0,0 +1,13 @@
+package br.com.centrocar.apurador.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
